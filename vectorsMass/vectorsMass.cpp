2bca4616948d91ba2953d4f57a3c368592114936
kaleriya <kaleriya@github.com>
2020-10-15 14:07:16 +0300

    добавление ф-ии isMonotoneDecreasing

--- vectorsMass/vectorsMass.cpp
+++ vectorsMass/vectorsMass.cpp
@@ -65,14 +65,27 @@ void random(int array[], const int SIZE)
 
 int isMonotoneDecreasing(int array[], const int SIZE)
 {
-	int res = 0;
+	int res = 0, max = 0;
 
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < SIZE ; i++)
 	{
-
+		if (array[i] > array[i + 1])
+		{
+			res++;
+			if (res > max)
+			{
+				max = res;
+				res = 0;
+			}
+		}
+	}
+	
+	if (res > max)
+	{
+		max = res;
 	}
 
-	return res;
+	return max;
 }
 
 int main()
@@ -95,13 +108,14 @@ int main()
 
 	//статический
 	const int SIZE = 10;
-	int array[SIZE] = { 3, 2, 4, 5, 6, 1, -9, 5, 6, 6 };
+	int array[SIZE] = { 3, 2, 1, -3, 6, 1, -9, 5, 6, 6 };
 
 
 	try
 	{
 		//std::cout << "Введите элементы динамического массива: " << std::endl;
 		//inArray(mas_array, size_array);
+
 		random(mas_array, size_array);
 		std::cout << "Вывод массива: " << std::endl;
 		outArray(mas_array, size_array);
@@ -114,6 +128,7 @@ int main()
 		outArray(mas_array, size_array);
 
 		std::cout << "Hаибольшую длинa монотонного убывающего фрагмента последовательности = ";
+		std::cout << isMonotoneDecreasing(mas_array, size_array) << std::endl;
 
 
 		delete[] mas_array;
@@ -128,11 +143,13 @@ int main()
 		std::cout << "Массив идет по возрастанию?" << std::endl;
 		std::cout << isOrderedArray(array, SIZE) << std::endl;
 
+		std::cout << "Hаибольшая длинa монотонного убывающего фрагмента последовательности = ";
+		std::cout << isMonotoneDecreasing(array, SIZE) << std::endl;
+
 		std::cout << "Перестановка соседних элементов массива: " << std::endl;
 		permutationArray(array, SIZE);
 		outArray(array, SIZE);
 
-		std::cout << "Hаибольшую длинa монотонного убывающего фрагмента последовательности = ";
 
 	}
 	catch (const std::exception& e)
